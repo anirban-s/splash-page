0ba013a48aa555a823efae14f9821aa21b777a47
anirban-s <baban.chrome@gmail.com>
2021-11-12 19:22:22 +0530

    background toggle

---
+++ script.js
@@ -0,0 +1,22 @@
+const { body } = document;
+
+function changeBackground(number) {
+    let previosBackground;
+    if (body.className) {
+        previosBackground = body.className;
+    }
+    body.className = '';
+    switch(number) {
+        case '1':
+            body.classList.add('background-1');
+            break;
+        case '2':
+            body.classList.add('background-2');
+            break;
+        case '3':
+            body.classList.add('background-3');
+            break;
+        default:
+            break;
+    }
+}
